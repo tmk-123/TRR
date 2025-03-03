a3744233e62a0a385d75b18289139ff8fb3a172f
Tran Manh Khiem <khiemmanhtran@gmail.com>
2025-03-03 15:37:26 +0700

    DFS de quy

--- 3_1.cpp
+++ 3_1.cpp
@@ -17,7 +17,7 @@ int main() {
     cin >> n;
     vector<vector<int>> v(n + 1, vector<int>(n + 1, 0));
     vector<bool> visited(n + 1, false);
-    
+
     for (int i = 1; i <= n; i++) {
         for (int j = 1; j <= n; j++) {
             cin >> v[i][j];
